38843a5f1a3f7a22d2f64157f527e964a5aa7b0e
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-04-02 16:55:47 -0700

    add function to js file that ignores all special characters and quotes.
    co-authored-by: Zinfira Safargalieva <zsafargalieva@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,35 @@
- 
+ function finish(inputText) {
+   console.log(inputText);
+  var firstSentence = inputText.toLowerCase();
+  var ignore = /[,.!?:'";<>@#$%ˆ&*1234567890 ]/g;
+  return firstSentence.replace(ignore, "");
+  
+  
+ };
+
+
+
+
+
+
+
+ $(document).ready(function() {
+   $('form#crypt').submit(function(event) {
+    event.preventDefault();
+
+    var inputText = $("input#inputText").val();
+    var result = finish(inputText);
+
+    $('.output').text(result);
+   });
+  });
+
+
+//  $(document).ready(function () {
+//    $("form#convert").submit(function (event) {
+//      event.preventDefault();
+//      var number = parseInt($("input#numInput").val());
+//      var result = convertToRoman(number);
+//      if (result) {
+//        $(".result").text(result);
+//      }
